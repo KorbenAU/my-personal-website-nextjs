ac49d02469321416ce59de04306e1c1d495187be
Korben <gaoyuan.hello@gmail.com>
2021-10-11 21:02:01 +1100

    add typing effect

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
+import Typewriter from 'typewriter-effect';
 
 const HeroSection = () => {
     return (
-        <div className={'relative top-1/3 left-1/4'}>
-            <div>
-                <span>{'Hi, I\'m'}</span>
-                <span className={'text-5xl'}>Korben</span>
+        <div className={'relative top-1/3 left-1/4 tracking-widest'}>
+            <div className={'text-black mb-14'}>
+                <span className={'text-3xl mr-6'}>{'Hi,  I\'m'}</span>
+                <span className={'text-8xl'}>Korben</span>
             </div>
-            <div>I design and develop web applications</div>
+
+            <div className={'text-3xl mb-8'}>I design and develop web applications</div>
+
+            <div className={'w-1/4'}>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString('The programing language and frameworks I\'m working on are ')
+                            .pauseFor(1000)
+                            .typeString('Java')
+                            .pauseFor(1000)
+                            .typeString(', JavaScript')
+                            .pauseFor(1000)
+                            .typeString(', PHP')
+                            .pauseFor(1000)
+                            .typeString(', C#')
+                            .pauseFor(1000)
+                            .typeString(', Python')
+                            .pauseFor(1000)
+                            .typeString(', TypeScript')
+                            .pauseFor(1000)
+                            .typeString(', HTML')
+                            .pauseFor(1000)
+                            .typeString(', CSS')
+                            .pauseFor(1000)
+                            .typeString(', React')
+                            .pauseFor(1000)
+                            .typeString(', Redux')
+                            .pauseFor(1000)
+                            .typeString(', .Net')
+                            .pauseFor(1000)
+                            .typeString(', Spring Boot')
+                            .pauseFor(1000)
+                            .typeString(', Express')
+                            .pauseFor(1000)
+                            .typeString(', Nextjs')
+                            .pauseFor(1000)
+                            .typeString(' ...')
+                            .start();
+                    }}
+                />
+            </div>
         </div>
     );
 };
